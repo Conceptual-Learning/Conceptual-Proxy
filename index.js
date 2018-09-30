7c304451cc415ddfdd9e7e12f365d86311d58998
lidor cohen <lidorcg@gmail.com>
2018-09-30 17:43:12 +0300

    use express with http-proxy

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var httpProxy = require("http-proxy");
+const express = require("express");
+const PORT = process.env.PORT || 5000;
 
-httpProxy
-  .createProxyServer({
-    target: "https://eu1.prisma.sh/lidor-cohen/conceptual-api/dev",
-    changeOrigin: true
-  })
-  .listen(443);
-console.log("listening on port 443");
+const app = express();
+
+const proxy = httpProxy.createProxyServer({
+  target: "https://eu1.prisma.sh/lidor-cohen/conceptual-api/dev",
+  changeOrigin: true
+});
+
+app.use("/api", function(req, res) {
+  proxy.web(req, res);
+});
+
+app.listen(PORT, function() {
+  console.log(`CORS-enabled web server listening on port ${PORT}`);
+});
