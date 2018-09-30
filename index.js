674e2b418a2d9c944b36535821ead5a277eb61d1
lidor cohen <lidorcg@gmail.com>
2018-09-30 17:24:16 +0300

    Revert "change port to env var"
    This reverts commit b70658925507874316f67cc409bcd2c2564fadba.

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-const express = require("express");
-var cors = require("cors");
-const proxy = require("express-http-proxy");
+var httpProxy = require("http-proxy");
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-const PRISMA_SERVICE = "https://eu1.prisma.sh/lidor-cohen/conceptual-api/dev";
-
-app.use(cors({ origin: PRISMA_SERVICE }));
-app.use("/", proxy(PRISMA_SERVICE));
-// app.use(express.static("public"));
-
-app.listen(PORT, function() {
-  console.log(`CORS-enabled web server listening on port ${PORT}`);
-});
+httpProxy
+  .createProxyServer({
+    target: "https://eu1.prisma.sh/lidor-cohen/conceptual-api/dev",
+    changeOrigin: true
+  })
+  .listen(443);
+console.log("listening on port 443");
